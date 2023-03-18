3cfa961d05a2933b008035e8fb9ec4cc0246e61e
Steve-bro <114855602+Steeve26@users.noreply.github.com>
2023-03-18 22:39:15 +0300

    styling added

--- src/components/formInput.jsx
+++ src/components/formInput.jsx
@@ -2,34 +2,16 @@ import React, { useState } from 'react'
 import styles from './formInput.module.css'
 
 export default function formInput(props) {
-  const {change, id, label, pass, errorMessage, ...input} = props
+  const {change, id, label, pass, errorMessage, required, ...input} = props
   const [focused, setFocused] = useState('')
-  // function handleshit(e, name) {
-  //   let val = e.target.value
-  //   change(name, val)
-  //   // if (!val) 
-  //   //   setError(`Field can't be blank`)
-  //   //   else{
-  //   //   setError('')
-  //   //   if (name === 'username'){
-  //   //     if (val.length <= 2)
-  //   //       setError('Username needs to be atleast 3 characters')}
-  //   //   if (name === 'password'){
-  //   //     if (val.length <= 7)
-  //   //       setError('Password needs to be atleast 8 characters')}
-  //   //   if (name === 'confirmPassword')
-  //   //     if (val !== pass)
-  //   //       setError(`Passwords don't match`)
-  //   //     }
-  // }
 
   return (
     <div className={styles.formInput}>
-      <label htmlFor={label}>{label}</label>
+      <label htmlFor={label} title={required && 'Required'}>{label}</label>
       <br/>
       <input id={label} {...input} focused={focused.toString()} onBlur={() => setFocused(false)}
-      onChange={(e) => change(e.target.name, e.target.value)} />
-      <br />
+      onChange={(e) => change(e.target.name, e.target.value)} autoComplete="off" />
+      <br/>
       <span className={styles.error}>{errorMessage}</span>
     </div>
   )
